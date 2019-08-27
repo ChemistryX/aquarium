74b55d6cd8cf7eac888a8c014aaac754e0b3478c
ChemistryX <starpiung@naver.com>
2019-08-27 17:25:26 +0900

    logic changes

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,6 +1,12 @@
-$(document).mousemove, function (e) {    
+var debug = false;
+
+window.addEventListener('mousemove', function(e) {
+    if (debug) {
+        console.log("mouse loc: " + e.pageX + " " + e.pageY);
+    }
+    
     $('.fish').offset({
-        left: e.pageX,
+        left: e.pageX - 156,
         top: e.pageY
     });
-};
+})
