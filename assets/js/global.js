5cb299145f1ae27ba05c4152d792787a1f7d401f
ChemistryX <starpiung@naver.com>
2019-08-31 14:35:50 +0900

    add trail, flip logic and default bg

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,12 +1,52 @@
-var debug = false;
-
-window.addEventListener('mousemove', function(e) {
-    if (debug) {
-        console.log("mouse loc: " + e.pageX + " " + e.pageY);
+window.addEventListener('load', () => {
+    var oldX = 0;
+    function handleFish(e) {
+        if (e.pageX < oldX) {
+            $('.fish').css({
+                'transform': 'scaleX(-1)'
+            })
+            $('.fish').offset({
+                left: e.pageX + 64,
+                top: e.pageY
+            });
+        } else if (e.pageX > oldX) {
+            $('.fish').css({
+                'transform': ''
+            })
+            $('.fish').offset({
+                left: e.pageX - 156,
+                top: e.pageY
+            });
+        }
+        oldX = e.pageX;
     }
-    
-    $('.fish').offset({
-        left: e.pageX - 156,
-        top: e.pageY
-    });
+
+    function handleMouseTrail(e) {
+        [1, .9, .8, .5, .1].forEach(function (i) {
+            var j = (1 - i) * 50;
+            var elem = document.createElement('div');
+            var size = Math.ceil(Math.random() * 15 * i) + 'px';
+            elem.style.position = 'fixed';
+            elem.style.top = e.pageY + Math.round(Math.random() * j - j / 2) + 'px';
+            elem.style.left = e.pageX + Math.round(Math.random() * j - j / 2) + 'px';
+            elem.style.width = size;
+            elem.style.height = size;
+            elem.style.background = 'hsla(' +
+                Math.round(Math.random() * 360) + ', ' +
+                '100%, ' +
+                '50%, ' +
+                i + ')';
+            elem.style.borderRadius = size;
+            elem.style.pointerEvents = 'none';
+            document.body.appendChild(elem);
+            window.setTimeout(function () {
+                document.body.removeChild(elem);
+            }, Math.round(Math.random() * i * 2000));
+        });
+    }
+
+    window.addEventListener('mousemove', handleFish);
+    console.log('handleFish initialized')
+    window.addEventListener('mousemove', handleMouseTrail);
+    console.log('handleMouseTrail initialized');
 })
